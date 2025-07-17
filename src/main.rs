5d91f2fe0386a1ac5867f1fce856786e2c399e3a
Raymond Yeung <raymond.yeung@email.com>
2025-07-18 00:25:55 +0800

    Coded main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        width: 70,
+        height: 70,
+    };
+
+    println!(
+        "The area of the rectangle is {} unit squared.",
+        area(&rect)
+    );
 }
